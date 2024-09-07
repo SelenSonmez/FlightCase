f29bbb9135fd8be1434cdd169956ea83bdae0274
selensonmez <selensonmez2001@gmail.com>
2024-09-07 06:36:46 +0300

    colors added

---
+++ src/styles/colors.ts
@@ -0,0 +1,21 @@
+const colors = {
+    primary: '#FF5733',
+    secondary: '#FFC300',
+    background: '#FFFFFF',
+    text: '#000000',
+    modalBackground: 'rgba(0,0,0,0.5)',
+    modalButton: 'rgba(254, 226, 226, 1)',
+    closeButtonText: 'rgba(127, 29, 29, 1)',
+    duration: 'rgba(194, 65, 12, 1)',
+    flightNumber: 'rgba(107, 114, 128, 1)',
+    borderColor: 'rgba(254, 215, 170, 1)',
+    backgroundColor: 'rgba(255, 247, 237, 1)',
+    card: 'rgba(229, 231, 235, 1)',
+    timeText: 'rgba(55, 65, 81, 1)',
+    buttonBackgroundColor:'rgba(234, 88, 12, 1)',
+    message:'rgba(75, 85, 99, 1)'
+
+  };
+  
+  export default colors;
+  
